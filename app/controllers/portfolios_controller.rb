ff7cb2356b69555f69378415394699ec763d6c83
viuneeva <viuneeva@gocardless.com>
2022-01-11 19:34:26 +0000

    implement create and update functionality for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,35 @@ class PortfoliosController < ApplicationController
   def index
     @portfolios = Portfolio.all
   end
+
+  def new
+    @portfolio = Portfolio.new
+  end
+
+  def edit
+    @portfolio = Portfolio.find(params[:id])
+  end
+
+  def create
+    @portfolio = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to portfolios_path, notice: "Portfolio item was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @portfolio = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: "The portfolio item was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
 end
