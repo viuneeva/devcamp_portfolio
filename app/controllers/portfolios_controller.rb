5613a6f7112a928f923548617c3fc0fb4ef19fb8
viuneeva <viuneeva@gocardless.com>
2022-01-12 22:20:28 +0000

    implemented destroy functionality for portfolio item

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -37,4 +37,13 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
+  def destroy
+    @portfolio = Portfolio.find(params[:id])
+    @portfolio.destroy
+
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: "Record was removed." }
+    end
+  end
 end
