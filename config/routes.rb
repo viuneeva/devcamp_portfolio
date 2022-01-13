6435d07e6eeac670a34935aeda02660b51e19be4
viuneeva <viuneeva@gocardless.com>
2022-01-13 22:35:37 +0000

    Customise portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :portfolios_path
+  resources :portfolios, except: [:show]
+  get "portfolio/:id", to: "portfolios#show", as: "show_portfolio"
   resources :blogs
 
   get "about-me", to: "pages#about"
