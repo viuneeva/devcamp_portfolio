6097f1a6b22b722dda30c5864aed3d73e76772aa
viuneeva <viuneeva@gocardless.com>
2022-01-16 18:39:54 +0000

    add toggle action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get "portfolio/:id", to: "portfolios#show", as: "show_portfolio"
-  resources :blogs
+  resources :blogs do
+    member do
+      post :toggle_status
+    end
+  end
 
   get "about-me", to: "pages#about"
   get "contact", to: "pages#contact"
