c7c19dc9b0884889447e3a2de2a846057d9979e5
viuneeva <viuneeva@gocardless.com>
2022-03-23 22:37:45 +0000

    integrate db relationships between blogs and topics

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,6 +2,7 @@ class Blog < ApplicationRecord
   enum status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
+  belongs_to :topic
 
   validates_presence_of :title, :body
 end
